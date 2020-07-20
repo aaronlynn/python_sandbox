411564aad5dd9f507cc3aa2688c2704431440566
Aaron Lynn <aaronclynn13@gmail.com>
2020-07-20 14:00:11 -0400

    Finished variables, strings, lists, tuples/sets, & dicts

--- python_sandbox_starter/strings.py
+++ python_sandbox_starter/strings.py
@@ -1,6 +1,37 @@
 # Strings in python are surrounded by either single or double quotation marks. Let's look at string formatting and some string methods
 
+name = 'Aaron'
+age = 22
+
+# Concatenate
+print('Hello, my name is ' + name + ' and I am ' + str(age))
 
 # String Formatting
 
+# Arguments by position
+print('Hello, my name is {name} and I am {age}'.format(name=name, age=age))
+
+# F-Strings (Python 3.6 and above)
+print(f'Hello, my name is {name} and I am {age}')
+
 # String Methods
+
+s = 'hello world'
+
+print('Capitalize string:\t' + s.capitalize())
+print('Make all uppercase:\t' + s.upper())
+print('Make all lowercase:\t' + s.lower())
+print('Swap case:\t\t' + s.swapcase())
+print('Get length:\t\t' + str(len(s)))
+print('Replace:\t\t' + s.replace('world', 'everyone'))
+
+sub ='h'
+print('Count:\t\t\t' + str(s.count(sub)))
+
+print(s.startswith('hello'))
+print(s.endswith('d'))
+print(s.split())
+print(s.find('r'))
+print(s.isalnum())
+print(s.isalpha())
+print(s.isnumeric())
--- python_sandbox_starter/tuples_sets.py
+++ python_sandbox_starter/tuples_sets.py
@@ -1,5 +1,47 @@
 # A Tuple is a collection which is ordered and unchangeable. Allows duplicate members.
  
+# Create tuple
+fruits = ('Apples', 'Oranges', 'Grapes')
+# fruits2 = tuple(('Apples', 'Oranges', 'Grapes'))
 
+# Single value needs trailing comma
+fruits2 = ('Apples',)
+
+# Get value
+print(fruits[1])
+
+# Can't change value (following line throws TypeError)
+#fruits[0] = 'Pears'
+
+# Delete tupe
+del fruits2
+
+# Get length
+print(len(fruits))
+
+###########################################################################################################
 
 # A Set is a collection which is unordered and unindexed. No duplicate members.
+
+# Create set
+fruits_set = {'Apples', 'Oranges', 'Mangos'}
+
+# Check if in set
+print('Apples' in fruits_set)
+
+# Add to set
+fruits_set.add('Grapes')
+
+# Remove from set
+fruits_set.remove('Grapes')
+
+# Add duplicate
+fruits_set.add('Apples')
+
+# Clear set
+# fruits_set.clear()
+
+# Delete set
+# del fruits_set
+
+print(fruits_set)
