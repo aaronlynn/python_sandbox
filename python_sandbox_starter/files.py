83332c1bf28e39ee22ac5664f5d51a0aa50a7c18
Aaron Lynn <aaronclynn13@gmail.com>
2020-07-20 16:12:06 -0400

    Completed Python Crash Course

--- python_sandbox_starter/files.py
+++ python_sandbox_starter/files.py
@@ -1,1 +1,24 @@
 # Python has functions for creating, reading, updating, and deleting files.
+
+# Open a file
+myFile = open('myfile.txt', 'w')
+
+# Get info
+print('Name:\t\t', myFile.name)
+print('Is closed:\t', myFile.closed)
+print('Mode:\t\t', myFile.mode)
+
+# Write to file
+myFile.write('I love Python')
+myFile.write(' and JavaScript')
+myFile.close()
+
+# Append to file
+myFile = open('myfile.txt', 'a')    # Must be set to 'a' for append, otherwise will write over existing file
+myFile.write(' I also like PHP')
+myFile.close()
+
+# Read from file
+myFile = open('myfile.txt', 'r+')
+text = myFile.read(100)
+print(text)
